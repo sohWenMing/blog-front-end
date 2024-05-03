0502afccc3d44c157d0d09b45195853504dfd7af
Soh Wen Ming <wenming.soh@gmail.com>
2024-05-04 01:05:30 +0800

    working on blogpost page

--- src/components/blogPosts.jsx
+++ src/components/blogPosts.jsx
@@ -1,16 +1,20 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import Post from './blogpost_widgets/post'
 
 export default function BlogPosts() {
     const navigate = useNavigate();
     const [posts, setPosts] = useState([]);
+
+
     useEffect(() => {
         async function getPosts() {
             try {
                 const response = await axios.get('http://localhost:3000/api/blog', {withCredentials: true})
                 console.log("response: ", response);
                 setPosts(response.data);
+                
                 
             }
             catch(error) {
@@ -23,7 +27,27 @@ export default function BlogPosts() {
         }
         getPosts();
     }, []);
+
     useEffect(() => {
-        console.log("data after setting: ", posts);
-    })
+        console.log("posts after load: ", posts);
+
+    }, [posts])
+
+   return (
+    <>
+    <ul>
+    {posts.map((post) => {
+        console.log("postId: ", post.id)
+        return (
+            <>
+           {<li key={post.id}><Post title={post.title}/></li>}
+            </>
+           
+        
+        )
+    })}
+    </ul> 
+    </>
+   )
+
 }
---
+++ src/components/blogpost_widgets/post.jsx
@@ -0,0 +1,8 @@
+export default function Post({title}) {
+    return (
+        <>
+        <h1>{title}</h1>
+        </>
+    )
+}
+
