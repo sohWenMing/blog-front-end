6167b6eb3fc7c37d3fc55c5a7cc4dae32c44707a
Wen Ming Soh <wenming.soh@gmail.com>
2024-05-04 11:22:46 +0800

    fixed user data in listing

--- src/components/blogPosts.jsx
+++ src/components/blogPosts.jsx
@@ -4,20 +4,22 @@ import axios from 'axios';
 import Post from './blogpost_widgets/post'
 import Loader from './widgets/loader'
 
+
 export default function BlogPosts() {
     const navigate = useNavigate();
     const [posts, setPosts] = useState([]);
+    const [username, setUsername] = useState([]);
     const [dataLoaded, setDataLoaded] = useState(false);
-
-
+    
     useEffect(() => {
         async function getPosts() {
             try {
-                const response = await axios.get('http://localhost:3000/api/blog', {withCredentials: true})
-                console.log("response: ", response);
-                setPosts(response.data);
+                const { data } = await axios.get('http://localhost:3000/api/blog', {withCredentials: true})
+                console.log("data: ", data);
+                setPosts(data.postJsonArray);
+                setUsername(data.username);
                 setDataLoaded(true);
-                              
+          
             }
             catch(error) {
                 console.error("error: ", error.response);
@@ -34,24 +36,26 @@ export default function BlogPosts() {
     }, []);
 
     useEffect(() => {
-        console.log("posts after load: ", posts);
-
-    }, [posts])
+        console.log("dataLoaded: ", dataLoaded)
+    }, [dataLoaded])
 
    return (
-    <div className="blogposts-list">
-    {posts.length !== 0 &&
-        <ul>
-        {posts.map((post) => {
-            console.log("postId: ", post.id)
-            return (   
-            <li key={post.id}><Post title={post.title} url={post.url} author={post.author}/></li>
-            )
-        })}
-        </ul> 
-    }
-    {!dataLoaded && <Loader />}
-    </div>
+    <>
+        {dataLoaded && <h1>{username} is logged in.</h1>}
+        <div className="blogposts-list">
+        {posts.length !== 0 &&
+            <ul>
+            {posts.map((post) => {
+                console.log("postId: ", post.id)
+                return (   
+                <li key={post.id}><Post title={post.title} url={post.url} author={post.author}/></li>
+                )
+            })}
+            </ul> 
+        }
+        {!dataLoaded && <Loader />}
+        </div>
+    </>
    )
 
 }
